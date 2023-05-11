b1210675168eced116642b52acea99451b017577
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-05-11 02:19:36 -0400

    creating card elemnt and render products

--- node.js
+++ node.js
@@ -1,1 +1,22 @@
-console.log('checking js')
+console.log('checking js');
+
+function createCardElement(product){
+    let card = document.createElement('div');
+    card.classList.add('card') //class=card
+    let h2 = document.createElement('h2')
+    h2.textContent = product.name;
+    let img = document.createElement('img')
+    img.src = product.image;
+    img.classList.add('prodImage')//class=prodImage
+    let p = document.createElement('p')
+    p.textContent =`${product.price} price`
+    
+    card.append(h2,img,p)
+    document.getElementById("product-collection").appendChild(card)
+
+}
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/products").then(res=>res.json()).then(products=> products.forEach(product=>createCardElement(product)))
+})
+//console.log( fetch("http://localhost:3000/products").then(res=>res.json())).then(data=>console.log(data))
+    
