1e96cc93e4a18c77bc8517d800b7dd9a3f45e13b
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-05-11 10:30:28 -0400

    updating wish list

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 console.log('checking js');
 
+
 function createCardElement(product){
     let card = document.createElement('div');
     card.classList.add('card') //class=card
@@ -12,7 +13,7 @@ function createCardElement(product){
     p.textContent =`${product.price} $`;
     let a = document.createElement('a')
     a.textContent = `rating ${product.rating}/10`;
-    a.setAttribute('id', 'my-new-element');
+    a.classList.add('rate');
     let button = document.createElement('button')
     button.classList.add('like-btn')
     button.id = product.id
@@ -20,6 +21,16 @@ function createCardElement(product){
     let p1 = document.createElement('p1')
     p1.textContent = product.wishlist;
     p1.classList.add('wishList');
+    p1.addEventListener('click',()=>{
+        if(p1.innerText==='♡'){
+        p1.innerText ='♥'
+        p1.classList.add('activatedheart')
+        }else{
+            p1.innerText ='♡' 
+        }
+
+        updateLike(product.id,product.wishlist)
+    })
     
     card.append(h2,img,p,a,button,p1)
     document.getElementById("product-collection").appendChild(card)
@@ -29,4 +40,19 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/products").then(res=>res.json()).then(products=> products.forEach(product=>createCardElement(product)))
 })
 //console.log( fetch("http://localhost:3000/products").then(res=>res.json())).then(data=>console.log(data))
-    
+   
+//updating like in wishlist
+function updateLike(id,newLikes){
+    fetch(`http://localhost:3000/products/${id}`,{
+      method :"PATCH",
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "likes" : newLikes
+      })
+    })
+  }
+  
